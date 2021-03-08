25661128efab2da46597fe119660e18467994a2b
nachmatt <nmatonte@gmail.com>
2021-03-08 17:00:44 -0300

    a face, instead of faces

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
-        {'This Magic Smart Brain will detect faces in your pictures. Give it a try.'}
+        {'This Magic Smart Brain will detect a face in your pictures. Give it a try.'}
       </p>
       <div className='center'>
         <div className='form center pa3 br3 shadow-5'>
